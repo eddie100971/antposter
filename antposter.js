aa695b074b16630ab97384d6aecaf64b02382696
Edward Chang <edwarc10@uci.edu>
2020-11-15 02:59:13 -0800

    Random math

--- antposter.js
+++ antposter.js
@@ -1,4 +1,19 @@
 
+var truths = 7;
+var truth_list = ["UCI has the best CS students.", "UCI's mascot is Peter the Anteater.", "The I in UCI stands for Irvine.",
+                  "The newest dining common is Brandywine.", "The two main on-campus dorms are Middle Earth and Mesa Court.",
+                  "You are not allowed to skateboard on Ring Road.", "Petr drops are awesome!"];
+
+var lie_list = ["UCI is the number 1 public school.", "UCI is located in NorCal.", "UCI's Ring Road often sells $1 boba.",
+                "The AntHill is the UCI bookstore.", "UCI has no Computer Science major.", "The original Petr dropper is still attending UCI.",
+                "The most popular club in UCI is UCI Dragonboat.", "I am petr."]
+
+function getRandTruth() {
+  
+  var random_truth = Math.floor((Math.random() * truths) + 1);
+  return truth_list[random_truth];
+}
+
 function postTruth() {
     var truth = document.getElementById("truthinput").value;
     console.log(truth);
@@ -12,6 +27,8 @@ function postTruth() {
         console.log(error);
       });
 }
+
+
 
 
 //function getTruth() {
