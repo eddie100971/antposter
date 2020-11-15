8a9cafe68efd77e10d34b61a165af84739c44eac
Edward Chang <edwarc10@uci.edu>
2020-11-15 01:16:34 -0800

    JS edits

--- antposter.js
+++ antposter.js
@@ -8,3 +8,17 @@ function getTruth() {
         console.log(error);
     });
 }
+
+function postTruth() {
+    var truth = document.getElementById("truthinput").value;
+    console.log(truth);
+    axios.post('http://127.0.0.1:5000/submission_truth', {
+        truth_sub: truth
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+}
